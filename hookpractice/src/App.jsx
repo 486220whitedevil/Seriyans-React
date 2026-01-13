df147bdd6ac83f1756be0fa88f33326a2e4b293d
Deepak Kewat <deepakkewat768@gmail.com>
2026-01-13 06:30:42 +0530

    feat: implement increment and decrement functionality in App component

--- hookpractice/src/App.jsx
+++ hookpractice/src/App.jsx
@@ -1,14 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <div className='min-h-screen w-full bg-gray-950'>
+  let increase = () => {
+    setCount(count+1)
+  }
 
+  let decrease = () => {
+    setCount(count-1)
+  }
+
+  let maxIncrease = () => {
+    let sr = {...count}
+    sr = count+5
+    setCount(sr)
+  }
+
+  let maxDecrease = () => {
+    let sr = {...count}
+    sr = count-5
+    setCount(sr)
+  }
+
+  return (
+    <div className='min-h-screen w-full bg-gray-950 p-10 flex justify-center flex-col'>
+      <div className='h-100 w-100 bg-gray-600 rounded-4xl mx-auto flex justify-center items-center '>
+        <h1 className='text-white font-bold text-8xl'>{count}</h1>
+      </div>
+      <div className='h-10 w-100 p-5  text-white flex justify-between items-center mt-15 mx-auto'>
+        <button onClick={increase} className='bg-gray-800 px-5 py-2 rounded-4xl cursor-pointer hover:bg-amber-600'>Increase</button>
+        <button onClick={decrease} className='bg-gray-800 px-5 py-2 rounded-4xl cursor-pointer hover:bg-amber-600'>Decrease</button>
+      </div>
+      <div className='h-10 w-100 p-5  text-white flex justify-between items-center mt-15 mx-auto'>
+        <button onClick={maxIncrease} className='bg-gray-800 px-5 py-2 rounded-4xl cursor-pointer hover:bg-amber-600'>Decrease by 5</button>
+        <button onClick={maxDecrease} className='bg-gray-800 px-5 py-2 rounded-4xl cursor-pointer hover:bg-amber-600'>Increase by 5</button>
+      </div>
     </div>
   )
 }
